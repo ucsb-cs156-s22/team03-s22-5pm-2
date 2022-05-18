274b4f677500b34d044fc8ebcc4f6f94f6307d55
Hyun Kyum Kim <hyunkyumkim@ucsb.edu>
2022-05-18 04:00:48 -0700

    hkk - added UCSBOrganization menu nav bar test

--- frontend/src/tests/components/Nav/AppNavbar.test.js
+++ frontend/src/tests/components/Nav/AppNavbar.test.js
@@ -237,6 +237,29 @@ describe("AppNavbar tests", () => {
 
     });
    
+    test("renders the organization menu correctly for an admin", async () => {
+
+        const currentUser = currentUserFixtures.adminUser;
+        const systemInfo = systemInfoFixtures.showingBoth;
+
+        const doLogin = jest.fn();
+
+        const {getByTestId  } = render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <AppNavbar currentUser={currentUser} systemInfo={systemInfo} doLogin={doLogin} />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        await waitFor(() => expect(getByTestId("appnavbar-organization-dropdown")).toBeInTheDocument());
+        const dropdown = getByTestId("appnavbar-organization-dropdown");
+        const aElement = dropdown.querySelector("a");
+        expect(aElement).toBeInTheDocument();
+        aElement?.click();
+        await waitFor( () => expect(getByTestId(/appnavbar-organization-list/)).toBeInTheDocument() );
+
+    })
 });
 
 
