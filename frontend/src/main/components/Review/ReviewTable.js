a9eff65c2e42184ed49cfbced3c6747d5c10118b
Jesus Oviedo <jaoviedo@ucsb.edu>
2022-05-19 01:02:16 -0700

    jo - removed problem underscore in ReviewTable

--- frontend/src/main/components/Review/ReviewTable.js
+++ frontend/src/main/components/Review/ReviewTable.js
@@ -1,6 +1,6 @@
 import OurTable, { ButtonColumn } from "main/components/OurTable"; 
 import { useBackendMutation } from "main/utils/useBackend";
-import { cellToAxiosParamsDelete, _onDeleteSuccess } from "main/utils/reviewUtils"
+import { cellToAxiosParamsDelete, onDeleteSuccess } from "main/utils/reviewUtils"
 // import { useNavigate } from "react-router-dom";
 import { hasRole } from "main/utils/currentUser";
 
