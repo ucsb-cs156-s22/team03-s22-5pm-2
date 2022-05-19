5594104cfbee4d13da992c02ccf3196ccbd67d1c
jacksonjude <jacksonjude@mac.com>
2022-05-18 17:46:50 -0700

    Added MenuItemIndexPage & tests, menuItemFixtures

---
+++ frontend/src/fixtures/menuItemFixtures.js
@@ -0,0 +1,27 @@
+const menuItemFixtures = {
+    oneMenuItem: {
+        "name": "Chicken Caesar Salad",
+        "diningCommonsCode": "carrillo",
+        "station": "Entrees"
+    },
+    threeMenuItems: [
+        {
+            "name": "Cream of Broccoli Soup (v)",
+            "diningCommonsCode": "portola",
+            "station": "Greens & Grains"
+        },
+        {
+            "name": "Tofu Banh Mi Sandwich (v)",
+            "diningCommonsCode": "ortega",
+            "station": "Entree Specials"
+        },
+        {
+            "name": "Hot Dog Caesar Salad",
+            "diningCommonsCode": "dlg",
+            "station": "Salad"
+        }
+    ]
+};
+
+
+export { menuItemFixtures };
---
+++ frontend/src/main/pages/MenuItem/MenuItemIndexPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+export default function MenuItemIndexPage() {
+
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Menu Item table goes here</h1>
+      </div>
+    </BasicLayout>
+  )
+}
