8cdd1d9940b762b2bbd405a01ef1e79e9cb086f6
Limin Ding <liminding@ucsb.edu>
2022-05-18 23:02:49 -0700

    removed changes on App.js and AppNavbar.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,6 @@ import TodosCreatePage from "main/pages/Todos/TodosCreatePage";
 import TodosEditPage from "main/pages/Todos/TodosEditPage";
 
 import DiningCommonsIndexPage from "main/pages/DiningCommons/DiningCommonsIndexPage";
-import RecommendationsIndexPage from "main/pages/Recommendations/RecommendationsIndexPage";
 
 import UCSBDatesIndexPage from "main/pages/UCSBDates/UCSBDatesIndexPage";
 import UCSBDatesCreatePage from "main/pages/UCSBDates/UCSBDatesCreatePage";
