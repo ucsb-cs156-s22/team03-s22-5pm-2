75e5ba59f1c165d19e5d77a735f6f7c938d16ae1
Hyun Kyum Kim <hyunkyumkim@ucsb.edu>
2022-05-18 17:46:38 -0700

    hkk - added tests for Index Page

---
+++ frontend/src/fixtures/ucsbOrganizationFixtures.js
@@ -0,0 +1,31 @@
+const ucsbOrganizationFixtures = {
+    oneDiningCommons: {
+        "orgCode": "ZPR",
+        "orgTranlsationShort": "ZETA PHI RHO",
+        "orgTranlsation": "ZETA PHI RHO",
+        "hasTakeOutMeal": false
+    },
+    threeCommons: [
+        {
+            "orgCode": "SKY",
+            "orgTranlsationShort": "SKYDIVING CLUB",
+            "orgTranlsation": "SKYDIVING CLUB AT UCSB",
+            "hasTakeOutMeal": false
+        },
+        {
+            "orgCode": "OSLI",
+            "orgTranlsationShort": "STUDENT LIFE",
+            "orgTranlsation": "OFFICE OF STUDENT LIFE",
+            "hasTakeOutMeal": false
+        },
+        {
+            "orgCode": "KRC",
+            "orgTranlsationShort": "KOREAN RADIO CL",
+            "orgTranlsation": "KOREAN RADIO CLUB",
+            "hasTakeOutMeal": false
+        } 
+    ]
+};
+
+
+export { ucsbOrganizationFixtures };
