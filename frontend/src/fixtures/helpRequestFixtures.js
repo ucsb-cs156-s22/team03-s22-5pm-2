904b222fc14b614abe0d117fcc0425efe0bfb39d
Anouki Panthagani <anouki@csil.cs.ucsb.edu>
2022-05-18 23:24:28 -0700

    ap- added fixtures

--- frontend/src/fixtures/helpRequestFixtures.js
+++ frontend/src/fixtures/helpRequestFixtures.js
@@ -22,7 +22,7 @@ const helpRequestFixtures = {
             "explanation": "blah2",
             "id": 0,
             "requestTime": "2022-05-18T23:18:21.837Z",
-            "requesterEmail": "string",
+            "requesterEmail": "anouki@ucsb.edu",
             "solved": true,
             "tableOrBreakoutRoom": "string",
             "teamId": "string"
