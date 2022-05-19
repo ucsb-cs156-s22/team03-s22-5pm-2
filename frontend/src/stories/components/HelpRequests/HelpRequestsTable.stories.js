1966c50722c118eb1e870afb551fc8eb172bebaf
Anouki Panthagani <anouki@csil.cs.ucsb.edu>
2022-05-18 21:05:42 -0700

    ap - added index placeholder

---
+++ frontend/src/stories/components/HelpRequests/HelpRequestsTable.stories.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import HelpRequestsTable from "main/components/HelpRequests/HelpRequestsTable";
+import { helpRequestsFixtures } from 'fixtures/helpRequestsFixtures';
+
+export default {
+    title: 'components/HelpRequests/HelpRequestsTable',
+    component: HelpRequestsTable
+};
+
+const Template = (args) => {
+    return (
+        <HelpRequestsTable {...args} />
+    )
+};
+
+export const Empty = Template.bind({});
+
+Empty.args = {
+    helprequests: []
+};
+
+export const ThreeRequests = Template.bind({});
+
+ThreeRequests.args = {
+    dates: helpRequestsFixtures.threeRequests
+};
+
+
+
+
