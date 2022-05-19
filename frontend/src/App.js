7d73a37eada8c023084a52a8e6b5a33deaebd846
Hyun Kyum Kim <hyunkyumkim@ucsb.edu>
2022-05-18 18:27:10 -0700

    hkk - added UCSBOrganization for App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -65,13 +65,7 @@ function App() {
           )
         }
 
-        {
-          hasRole(currentUser, "ROLE_USER") && (
-            <>
-              <Route exact path="/ucsborganization/list" element={<UCSBOrganizationIndexPage />} />
-            </>
-          )
-        }
+        
 
       </Routes>
     </BrowserRouter>
