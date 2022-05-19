6a906b0f72187c478d01279652d95a4f856a0f91
Limin Ding <liminding@ucsb.edu>
2022-05-18 23:55:09 -0700

    added extra test cases

--- frontend/src/tests/pages/Recommendations/RecommendationIndexPage.test.js
+++ frontend/src/tests/pages/Recommendations/RecommendationIndexPage.test.js
@@ -87,9 +87,9 @@ describe("RecommendationIndexPage tests", () => {
             </QueryClientProvider>
         );
 
-        await waitFor(  () => { expect(getByTestId(`${testId}-cell-row-0-col-professorEmail`)).toHaveTextContent("phtcon@ucsb.edu"); } );
-        expect(getByTestId(`${testId}-cell-row-1-col-professorEmail`)).toHaveTextContent("richert@ucsb.edu");
-        expect(getByTestId(`${testId}-cell-row-2-col-professorEmail`)).toHaveTextContent("pon@ucsb.edu");
+        await waitFor(  () => { expect(queryByTestId(`${testId}-cell-row-0-col-professorEmail`)).toHaveTextContent("phtcon@ucsb.edu"); } );
+        expect(queryByTestId(`${testId}-cell-row-1-col-professorEmail`)).toHaveTextContent("richert@ucsb.edu");
+        expect(queryByTestId(`${testId}-cell-row-2-col-professorEmail`)).toHaveTextContent("pon@ucsb.edu");
 
     });
 
@@ -106,9 +106,9 @@ describe("RecommendationIndexPage tests", () => {
             </QueryClientProvider>
         );
 
-        await waitFor(  () => { expect(getByTestId(`${testId}-cell-row-0-col-professorEmail`)).toHaveTextContent("phtcon@ucsb.edu"); } );
-        expect(getByTestId(`${testId}-cell-row-1-col-professorEmail`)).toHaveTextContent("richert@ucsb.edu");
-        expect(getByTestId(`${testId}-cell-row-2-col-professorEmail`)).toHaveTextContent("pon@ucsb.edu");
+        await waitFor(  () => { expect(queryByTestId(`${testId}-cell-row-0-col-professorEmail`)).toHaveTextContent("phtcon@ucsb.edu"); } );
+        expect(queryByTestId(`${testId}-cell-row-1-col-professorEmail`)).toHaveTextContent("richert@ucsb.edu");
+        expect(queryByTestId(`${testId}-cell-row-2-col-professorEmail`)).toHaveTextContent("pon@ucsb.edu");
 
     });
 
