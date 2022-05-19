ed2c82931bf720c7317b4f9a073d8fb0f96711e0
ryanlavu <72892837+ryanlavu@users.noreply.github.com>
2022-05-18 21:06:42 -0700

    RV - added article appnavbar test

--- frontend/src/tests/components/Nav/AppNavbar.test.js
+++ frontend/src/tests/components/Nav/AppNavbar.test.js
@@ -334,6 +334,30 @@ describe("AppNavbar tests", () => {
         aElement?.click();
         await waitFor( () => expect(getByTestId(/appnavbar-review-list/)).toBeInTheDocument() );
     });
+
+    test("renders the Articles menu correctly for a user", async () => {
+
+        const currentUser = currentUserFixtures.userOnly;
+        const systemInfo = systemInfoFixtures.showingBoth;
+
+        const doLogin = jest.fn();
+
+        const {getByTestId  } = render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <AppNavbar currentUser={currentUser} systemInfo={systemInfo} doLogin={doLogin} />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        await waitFor(() => expect(getByTestId("appnavbar-article-dropdown")).toBeInTheDocument());
+        const dropdown = getByTestId("appnavbar-article-dropdown");
+        const aElement = dropdown.querySelector("a");
+        expect(aElement).toBeInTheDocument();
+        aElement?.click();
+        await waitFor( () => expect(getByTestId("appnavbar-article-list")).toBeInTheDocument() );
+
+    });
    
 });
 
