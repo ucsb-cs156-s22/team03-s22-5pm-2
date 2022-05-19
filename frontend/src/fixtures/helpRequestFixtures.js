48e458d2fe9f994a2214662583623db7eccab6ec
Anouki Panthagani <anouki@csil.cs.ucsb.edu>
2022-05-18 23:18:03 -0700

    ap - added fixtures file

---
+++ frontend/src/fixtures/helpRequestFixtures.js
@@ -0,0 +1,43 @@
+const helpRequestFixtures = {
+    oneRequest: {
+        "explanation": "blah",
+        "id": 0,
+        "requestTime": "2022-05-18T23:18:21.837Z",
+        "requesterEmail": "string",
+        "solved": true,
+        "tableOrBreakoutRoom": "string",
+        "teamId": "string"
+      },
+    threRequests: [
+        {
+            "explanation": "blah1",
+            "id": 0,
+            "requestTime": "2022-05-18T23:18:21.837Z",
+            "requesterEmail": "string",
+            "solved": true,
+            "tableOrBreakoutRoom": "string",
+            "teamId": "string"
+          },
+          {
+            "explanation": "blah2",
+            "id": 0,
+            "requestTime": "2022-05-18T23:18:21.837Z",
+            "requesterEmail": "string",
+            "solved": true,
+            "tableOrBreakoutRoom": "string",
+            "teamId": "string"
+          },
+          {
+            "explanation": "blah3",
+            "id": 0,
+            "requestTime": "2022-05-18T23:18:21.837Z",
+            "requesterEmail": "string",
+            "solved": true,
+            "tableOrBreakoutRoom": "string",
+            "teamId": "string"
+          } 
+    ]
+};
+
+
+export { helpRequestFixtures };
