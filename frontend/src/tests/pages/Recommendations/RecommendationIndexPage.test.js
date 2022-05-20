52e0f68163ba61c5419c08f3f56cc8a0e590618f
Limin Ding <liminding@ucsb.edu>
2022-05-19 17:07:17 -0700

    fixed minor mistake

--- frontend/src/tests/pages/Recommendations/RecommendationIndexPage.test.js
+++ frontend/src/tests/pages/Recommendations/RecommendationIndexPage.test.js
@@ -45,7 +45,7 @@ describe("RecommendationIndexPage tests", () => {
     test("renders without crashing for regular user", () => {
         setupUserOnly();
         const queryClient = new QueryClient();
-        axiosMock.onGet("/api/recommendations/all").reply(200, []);
+        axiosMock.onGet("/api/Recommendations/all").reply(200, []);
 
         render(
             <QueryClientProvider client={queryClient}>
@@ -61,7 +61,7 @@ describe("RecommendationIndexPage tests", () => {
     test("renders without crashing for admin user", () => {
         setupAdminUser();
         const queryClient = new QueryClient();
-        axiosMock.onGet("/api/recommendations/all").reply(200, []);
+        axiosMock.onGet("/api/Recommendations/all").reply(200, []);
 
         render(
             <QueryClientProvider client={queryClient}>
@@ -155,7 +155,7 @@ describe("RecommendationIndexPage tests", () => {
 
         await waitFor(() => { expect(getByTestId(`${testId}-cell-row-0-col-id`)).toBeInTheDocument(); });
 
-       expect(getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent(0); 
+       expect(getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent(1); 
 
 
         const deleteButton = getByTestId(`${testId}-cell-row-0-col-Delete-button`);
